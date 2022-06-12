7497f34b8c3cb27c57225aca134e8d4ef4d423dd
TRBC - The Real Bitcoin Club <trbc@bitcoinmap.cash>
2022-06-12 17:35:54 -0400

    254014

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "be0c54009020ed548f17b8b963c255f7",
-"index.html": "ee813b5f768596193d3afed20313f732",
-"/": "ee813b5f768596193d3afed20313f732",
+  "version.json": "1f1e77bbb16fd7eb9421a9acf4351d11",
+"index.html": "297143af753106e676c98bd9b4468930",
+"/": "297143af753106e676c98bd9b4468930",
 "jssocials-theme-plain.css": "5ee9d7623ad0c2a3ed195d102a7e573a",
 "bitcoinmap_es.html": "908d6da151a3ebb65acfd95421da220a",
-"main.dart.js": "928e7ed9feb92a6ee2f261886b310a5a",
+"main.dart.js": "1a037d46b06d96c462a7f65fad05a812",
 "old_favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "charts.js": "beb2a4be5a45038bc554feb30f353e7f",
